1e0ebd81a0e8497c5bf88ab0c6c9ff828bf7ac00
Allyson Le <atl@usc.edu>
2025-11-11 20:54:43 +0000

    "part 1"

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <iostream>
 
 #endif
 
@@ -8,11 +9,45 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int length(Node * root){
+    if(root == NULL){
+        return 0;
+    } else if (!(root->left) && !(root->right)){
+        return 1;
+    }
+    int left = length(root->left);
+    int right = length(root->right);
 
+    if(left > right){
+        return left + 1;
+    } else {
+        return right + 1;
+    }
+
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+
+    if(root == NULL){
+        return true;
+    } else if (!(root->left) && !(root->right)){
+        return true;
+    }
+
+    bool leftP = equalPaths(root->left);
+    bool rightP = equalPaths(root->right);
+
+    int leftL = length(root->left);
+    int rightL = length(root->right);
+
+    if(leftP && rightP){
+        if (leftL == rightL || root->left == NULL || root->right == NULL){
+            return true;
+        }
+    }
+
+    return false;
 
 }
 
